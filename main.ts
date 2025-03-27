bcab237c2aeae994085de261802956463536752f
Dan Sto. Domingo <danielesto.domingo@gmail.com>
2025-03-27 11:07:35 +0800

    add annotate comments command

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ import { FileHandler } from './file-handler';
 import { patchDefaultSuggester } from './suggester-patcher';
 import { ARTICLE_VIEW_TYPE, ArticleView } from './components/article-view';
 import { NOTES_VIEW_TYPE, NotesView } from './components/notes-view';
+import { COMMENTS_VIEW_TYPE, CommentsView } from './components/comments-view';
 
 export default class ArticleSuggestPlugin extends Plugin {
     private articleSuggest: ArticleSuggest;
@@ -23,6 +24,16 @@ export default class ArticleSuggestPlugin extends Plugin {
             return new NotesView(leaf);
         });
         
+        this.registerView(COMMENTS_VIEW_TYPE, (leaf) => {
+            return new CommentsView(leaf);
+        });
+        
+        this.addCommand({
+            id: 'annotate-comments',
+            name: 'Annotate comments',
+            callback: () => this.openCommentsView()
+        });
+        
         patchDefaultSuggester(this.app);
 
         this.registerEvent(
@@ -38,6 +49,17 @@ export default class ArticleSuggestPlugin extends Plugin {
                 this.fileHandler.checkIfFileStillOpen();
             })
         );
+    }
+    
+    async openCommentsView() {
+        const leaf = this.app.workspace.getRightLeaf(false);
+        if (leaf) {
+            leaf.setViewState({
+                type: COMMENTS_VIEW_TYPE,
+                active: true
+            });
+            this.app.workspace.revealLeaf(leaf);
+        }
     }
 
     onunload() {
