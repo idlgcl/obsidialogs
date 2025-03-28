21eea8b2cb1754955d37efd04f11f8fb1bd3694c
Dan Sto. Domingo <danielesto.domingo@gmail.com>
2025-03-28 22:38:14 +0800

    add tabs

--- components/right-panel-list-view.ts
+++ components/right-panel-list-view.ts
@@ -1,6 +1,9 @@
 import { Component } from "obsidian";
+import { TabsComponent, TabItem } from "./tabs-component";
+import { CommentsTab } from "./comments-tab";
+import { NotesTab } from "./notes-tab";
 
-export interface SidePanelListViewOptions {
+export interface RightPanelListViewOptions {
     container: HTMLElement;
     onSelectItem: () => void;
 }
@@ -9,25 +12,63 @@ export class RightPanelListView extends Component {
     private container: HTMLElement;
     private contentEl: HTMLElement;
     private onSelectItem: () => void;
+    private tabsComponent: TabsComponent;
+    private commentsTab: CommentsTab;
+    private notesTab: NotesTab;
+    private tabContentEl: HTMLElement;
     
-    constructor(options: SidePanelListViewOptions) {
+    constructor(options: RightPanelListViewOptions) {
         super();
         this.container = options.container;
         this.onSelectItem = options.onSelectItem;
         this.createView();
     }
     
-    createView() {
+    private createView(): void {
         this.contentEl = this.container.createDiv({ cls: 'side-panel-list-view' });
         
-        this.contentEl.createEl('h3', { text: 'List View' });
+        this.contentEl.createEl('h3', { text: 'Idealogs' });
         
-        const itemEl = this.contentEl.createDiv({ cls: 'side-panel-list-item' });
-        itemEl.setText('Sample Item - Click me');
+        const tabs: TabItem[] = [
+            { id: 'comments', label: 'Comments' },
+            { id: 'notes', label: 'Notes' }
+        ];
         
-        itemEl.addEventListener('click', () => {
-            this.onSelectItem();
+        this.tabsComponent = new TabsComponent({
+            container: this.contentEl,
+            tabs: tabs,
+            activeTabId: 'comments',
+            onTabChange: (tabId) => this.handleTabChange(tabId)
         });
+        
+        this.addChild(this.tabsComponent);
+        
+        this.tabContentEl = this.contentEl.createDiv({ cls: 'idl-tab-contents' });
+        
+        this.commentsTab = new CommentsTab({
+            container: this.tabContentEl,
+            onSelectComment: () => this.onSelectItem()
+        });
+        
+        this.notesTab = new NotesTab({
+            container: this.tabContentEl,
+            onSelectNote: () => this.onSelectItem()
+        });
+        
+        this.addChild(this.commentsTab);
+        this.addChild(this.notesTab);
+        
+        this.handleTabChange('comments');
+    }
+    
+    private handleTabChange(tabId: string): void {
+        if (tabId === 'comments') {
+            this.commentsTab.show();
+            this.notesTab.hide();
+        } else if (tabId === 'notes') {
+            this.commentsTab.hide();
+            this.notesTab.show();
+        }
     }
     
     show() {
