1639dd936d2b460ca9ed4d497721e61908ee8930
Dan Sto. Domingo <danielesto.domingo@gmail.com>
2025-09-30 23:03:54 +0800

    set target test files and coverage percent

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,30 @@
 /** @type {import('jest').Config} */
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
   roots: ["<rootDir>/tests"],
+  setupFilesAfterEnv: ["<rootDir>/tests/setup.ts"],
   moduleNameMapper: {
-    "^obsidian$": "<rootDir>/tests/__mocks__/obsidian.ts", 
+    "^obsidian$": "<rootDir>/tests/__mocks__/obsidian.ts",
+    "^components/(.*)$": "<rootDir>/components/$1",
+    "^utils/(.*)$": "<rootDir>/utils/$1",
   },
+  collectCoverageFrom: [
+    "utils/**/*.ts",
+    "components/suggester.ts",
+    "components/article-input.ts",
+    "!**/*.d.ts",
+    "!**/node_modules/**",
+    "!**/tests/**",
+    "!**/coverage/**",
+  ],
+coverageThreshold: {
+    "global": {
+      "lines": 70
+    },
+    "utils/**/*.ts": {
+      "lines": 70,
+      "functions": 70
+    }
+  }
 };
