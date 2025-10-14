ffdeb79442618df7aa965b707c747535c473d6be
Dan Sto. Domingo <danielesto.domingo@gmail.com>
2025-10-15 01:49:11 +0800

    wip

--- main.ts
+++ main.ts
@@ -1,17 +1,41 @@
 import { Plugin } from "obsidian";
 import { ArticleSuggest } from "./components/suggester";
-import { patchDefaultSuggester } from "./utils/suggester-patcher";
+import { patchDefaultSuggester } from "./utils/suggest-patcher";
 import { ApiService } from "./utils/api";
+import {
+  WritingLinkHandler,
+  CommonLinkHandler,
+  patchLinkOpening,
+} from "./utils/link-handlers";
 
 export default class IdealogsPlugin extends Plugin {
   private articleSuggest: ArticleSuggest;
   private apiService: ApiService;
+  private writingLinkHandler: WritingLinkHandler;
+  private commonLinkHandler: CommonLinkHandler;
+  private restoreLinkOpening: (() => void) | null = null;
 
   async onload() {
     this.apiService = new ApiService();
+    this.writingLinkHandler = new WritingLinkHandler();
+    this.commonLinkHandler = new CommonLinkHandler();
+
     this.articleSuggest = new ArticleSuggest(this, this.apiService);
     this.registerEditorSuggest(this.articleSuggest);
 
     patchDefaultSuggester(this.app);
+
+    this.restoreLinkOpening = patchLinkOpening(
+      this.app,
+      this.writingLinkHandler,
+      this.commonLinkHandler
+    );
+  }
+
+  onunload() {
+    // Restore original openLinkText function
+    if (this.restoreLinkOpening) {
+      this.restoreLinkOpening();
+    }
   }
 }
