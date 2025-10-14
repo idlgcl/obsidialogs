4c2d431a873d89970f816b92455995b931200e6f
Dan Sto. Domingo <danielesto.domingo@gmail.com>
2025-10-15 02:36:26 +0800

    auto delete idealogs files

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { Plugin } from "obsidian";
+import { Plugin, TFile } from "obsidian";
 import { ArticleSuggest } from "./components/suggester";
 import { patchDefaultSuggester } from "./utils/suggest-patcher";
 import { ApiService } from "./utils/api";
@@ -14,6 +14,7 @@ export default class IdealogsPlugin extends Plugin {
   private writingLinkHandler: WritingLinkHandler;
   private commonLinkHandler: CommonLinkHandler;
   private restoreLinkOpening: (() => void) | null = null;
+  private previousFile: TFile | null = null;
 
   async onload() {
     this.apiService = new ApiService();
@@ -30,6 +31,39 @@ export default class IdealogsPlugin extends Plugin {
       this.writingLinkHandler,
       this.commonLinkHandler
     );
+
+    this.registerEvent(
+      this.app.workspace.on("active-leaf-change", () => {
+        this.handleFileChange();
+      })
+    );
+  }
+
+  private async handleFileChange(): Promise<void> {
+    const currentFile = this.app.workspace.getActiveFile();
+
+    if (this.previousFile && this.previousFile !== currentFile) {
+      const isStillOpen = this.app.workspace
+        .getLeavesOfType("markdown")
+        .some((leaf) => {
+          const file = leaf.view.getState()?.file;
+          return file === this.previousFile?.path;
+        });
+
+      if (!isStillOpen && this.isIdealogsArticle(this.previousFile.basename)) {
+        try {
+          await this.app.vault.delete(this.previousFile);
+        } catch (error) {
+          console.error("Error deleting Idealogs article:", error);
+        }
+      }
+    }
+
+    this.previousFile = currentFile;
+  }
+
+  private isIdealogsArticle(basename: string): boolean {
+    return /^(Tx|Fx|Ix)/.test(basename);
   }
 
   onunload() {
