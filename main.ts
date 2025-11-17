0323aef078e851931df2d72113b12dcc18d4e669
Dan Sto. Domingo <danielesto.domingo@gmail.com>
2025-11-18 01:41:59 +0800

    wip

--- main.ts
+++ main.ts
@@ -1,40 +1,15 @@
 import {
   Plugin,
-  TFile,
-  MarkdownView,
   PluginSettingTab,
   App,
   Setting,
   Notice,
+  MarkdownView,
+  TFile,
 } from "obsidian";
-import { ArticleSuggest } from "./utils/suggester";
-import { patchDefaultSuggester } from "./utils/suggest-patcher";
+import { EditorView } from "@codemirror/view";
 import { ApiService } from "./utils/api";
 import { ArticleSearchModal } from "./components/ArticleSearchModal";
-import {
-  NoteLinkHandler,
-  CommonLinkHandler,
-  patchLinkOpening,
-} from "./utils/link-handlers";
-import { IdealogsFileTracker } from "./utils/idealogs-file-tracker";
-import {
-  CommentParser,
-  Comment,
-  NoteLinkInfo,
-  detectNoteLink,
-} from "./utils/parsers";
-import {
-  ANNOTATION_FORM_VIEW,
-  AnnotationFormView,
-} from "components/AnnotationFormView";
-import {
-  Annotation,
-  AnnotationService,
-  WebAnnotation,
-} from "./utils/annotation-service";
-import { AnnotationHighlighter } from "./utils/annotation-highlighter";
-import { EditorView } from "@codemirror/view";
-import { SplitManager } from "./utils/split-manager";
 
 interface IdealogsSettings {
   enableLogs: boolean;
@@ -45,111 +20,24 @@ const DEFAULT_SETTINGS: IdealogsSettings = { enableLogs: false };
 export default class IdealogsPlugin extends Plugin {
   settings: IdealogsSettings;
   apiService: ApiService;
-  annotationService: AnnotationService;
-  private articleSuggest: ArticleSuggest;
-  private fileTracker: IdealogsFileTracker;
-  private splitManager: SplitManager;
-  private noteLinkHandler: NoteLinkHandler;
-  private commonLinkHandler: CommonLinkHandler;
-  private annotationHighlighter: AnnotationHighlighter;
-  private restoreLinkOpening: (() => void) | null = null;
-  private previousFile: TFile | null = null;
-  private commentParser: CommentParser;
-  private cursorCheckInterval: number | null = null;
-  private lastCursorLine = -1;
-  private lastCursorCh = -1;
-  private editorChangeDebounceTimer: number | null = null;
 
   async onload() {
     await this.loadSettings();
 
+    // Initialize core services
     this.apiService = new ApiService();
-    this.fileTracker = new IdealogsFileTracker();
-    this.splitManager = new SplitManager(
-      this.app,
-      this.fileTracker,
-      this.apiService
-    );
-    this.commentParser = new CommentParser();
-    this.annotationService = new AnnotationService(this.app);
-    this.annotationHighlighter = new AnnotationHighlighter(this.app);
-    this.annotationHighlighter.setDependencies(
-      this.apiService,
-      this.fileTracker
-    );
 
-    this.noteLinkHandler = new NoteLinkHandler(
-      this.app,
-      this.apiService,
-      this.fileTracker,
-      this.annotationService,
-      this.annotationHighlighter,
-      this.splitManager
-    );
-
-    this.commonLinkHandler = new CommonLinkHandler(
-      this.app,
-      this.apiService,
-      this.fileTracker
-    );
-
-    this.registerEditorExtension(this.createArticleTriggerExtension());
-    this.registerEditorExtension(this.createCommentClickExtension());
-    this.registerEditorExtension(this.createNoteClickExtension());
-
-    this.registerView(ANNOTATION_FORM_VIEW, (leaf) => {
-      const view = new AnnotationFormView(leaf);
-      view.setSplitManager(this.splitManager);
-      view.setAnnotationService(this.annotationService);
-      view.setAnnotationHighlighter(this.annotationHighlighter);
-      return view;
-    });
-
-    // Patches
-    patchDefaultSuggester(this.app);
-
-    this.restoreLinkOpening = patchLinkOpening(
-      this.app,
-      this.noteLinkHandler,
-      this.commonLinkHandler
-    );
-
-    // Events
-    this.registerEvent(
-      this.app.workspace.on("active-leaf-change", () => {
-        this.handleFileChange();
-      })
-    );
-
-    this.registerEvent(
-      this.app.workspace.on("editor-change", () => {
-        this.debouncedCheckCursorInComment();
-      })
-    );
-
-    // TODO : add it back
-    // Validate annotations when file is modified
-    // this.registerEvent(
-    //   this.app.vault.on("modify", async (file) => {
-    //     if (file instanceof TFile && file.extension === "md") {
-    //       await this.annotationService.validateAllAnnotations(file.path);
-    //     }
-    //   })
-    // );
-
-    // Check cursor position every 200ms
-    this.cursorCheckInterval = window.setInterval(() => {
-      this.checkCursorInComment();
-    }, 200);
-
-    // TODO : add it back
-    // markdown processor for annotation in preview/read mode only
-    // this.registerMarkdownPostProcessor(async (el, ctx) => {
-    //   await this.renderAnnotations(el, ctx.sourcePath);
-    // });
+    // CodeMirror extensions
+    this.registerEditorExtension(this.createArticleLookupExtension());
 
     // Settings tab
     this.addSettingTab(new IdealogsSettingTab(this.app, this));
+
+    this.registerEvent(
+      this.app.workspace.on("file-open", (file: TFile) => {
+        console.log("test", file.basename);
+      })
+    );
   }
 
   async loadSettings() {
@@ -160,159 +48,7 @@ export default class IdealogsPlugin extends Plugin {
     await this.saveData(this.settings);
   }
 
-  private async renderAnnotations(
-    container: HTMLElement,
-    sourcePath: string
-  ): Promise<void> {
-    try {
-      this.annotationHighlighter.transformAllIdealogsLinks(
-        container,
-        sourcePath
-      );
-
-      const annotations = await this.annotationService.loadAnnotations(
-        sourcePath
-      );
-
-      const allAnnotations: Annotation[] = [];
-
-      for (const commentId in annotations.comments) {
-        allAnnotations.push(annotations.comments[commentId]);
-      }
-
-      for (const noteId in annotations.notes) {
-        allAnnotations.push(annotations.notes[noteId]);
-      }
-
-      if (allAnnotations.length > 0) {
-        this.annotationHighlighter.highlightAnnotations(
-          container,
-          allAnnotations
-        );
-      }
-    } catch (error) {
-      console.error("[Idealogs] Error rendering annotations:", error);
-    }
-  }
-
-  private createCommentClickExtension() {
-    const handleCommentTitleClick = this.handleCommentTitleClick.bind(this);
-    const app = this.app;
-    const commentParser = new CommentParser();
-
-    return EditorView.domEventHandlers({
-      mousedown: (event: MouseEvent, view: EditorView) => {
-        if (!event.ctrlKey && !event.metaKey) {
-          return false;
-        }
-
-        try {
-          const pos = view.posAtCoords({
-            x: event.clientX,
-            y: event.clientY,
-          });
-
-          if (pos === null) {
-            return false;
-          }
-
-          const line = view.state.doc.lineAt(pos);
-          const lineText = line.text;
-          const charOffset = pos - line.from;
-
-          const activeView = app.workspace.getActiveViewOfType(MarkdownView);
-          if (!activeView?.file) {
-            return false;
-          }
-
-          const commentWithPos = commentParser.findCommentAtPosition(
-            lineText,
-            charOffset,
-            activeView.file.name,
-            activeView.file.path
-          );
-
-          if (commentWithPos) {
-            const commentText = lineText.substring(
-              commentWithPos.startPos,
-              commentWithPos.endPos
-            );
-            const titleEndPosInComment = commentText.indexOf(".");
-
-            if (titleEndPosInComment !== -1) {
-              const titleEndPos =
-                commentWithPos.startPos + titleEndPosInComment;
-              if (charOffset <= titleEndPos) {
-                handleCommentTitleClick(commentWithPos);
-                return true;
-              }
-            }
-          }
-        } catch (error) {
-          console.error("[Idealogs] Error handling click:", error);
-        }
-
-        return false;
-      },
-    });
-  }
-
-  private createNoteClickExtension() {
-    const handleNoteLinkClick = this.handleNoteLinkClick.bind(this);
-    const app = this.app;
-
-    return EditorView.domEventHandlers({
-      mousedown: (event: MouseEvent, view: EditorView) => {
-        try {
-          const pos = view.posAtCoords({
-            x: event.clientX,
-            y: event.clientY,
-          });
-
-          if (pos === null) {
-            return false;
-          }
-
-          const line = view.state.doc.lineAt(pos);
-          const lineText = line.text;
-
-          // check for note link
-          if (!lineText.match(/\[\[@[TFI]x[^\]]+\]\]/)) {
-            return false;
-          }
-
-          const activeView = app.workspace.getActiveViewOfType(MarkdownView);
-          if (!activeView?.file) {
-            return false;
-          }
-
-          const noteLinkInfo = detectNoteLink(
-            lineText,
-            activeView.file.name,
-            activeView.file.path
-          );
-
-          if (noteLinkInfo) {
-            const charOffset = pos - line.from;
-            const linkStart = lineText.indexOf(noteLinkInfo.linkText);
-            const linkEnd = linkStart + noteLinkInfo.linkText.length;
-
-            if (charOffset >= linkStart && charOffset <= linkEnd) {
-              handleNoteLinkClick(noteLinkInfo);
-              // NoteLinkHandler also trigger
-              return false;
-            }
-          }
-        } catch (error) {
-          console.error("[Idealogs] Error handling note click:", error);
-        }
-
-        return false;
-      },
-    });
-  }
-
-  private createArticleTriggerExtension() {
+  private createArticleLookupExtension() {
     const app = this.app;
     const apiService = this.apiService;
 
@@ -348,7 +84,7 @@ export default class IdealogsPlugin extends Plugin {
             const beforeCursor = currentLine.substring(0, cursor.ch + 1);
 
             if (beforeCursor.endsWith("[[@")) {
-              // Remove  '[[@]]'
+              // Remove '[[@]]'
               editor.replaceRange(
                 "",
                 { line: cursor.line, ch: cursor.ch - 2 },
@@ -372,206 +108,7 @@ export default class IdealogsPlugin extends Plugin {
     });
   }
 
-  private debouncedCheckCursorInComment(): void {
-    if (this.editorChangeDebounceTimer !== null) {
-      window.clearTimeout(this.editorChangeDebounceTimer);
-    }
-
-    this.editorChangeDebounceTimer = window.setTimeout(() => {
-      this.checkCursorInComment(true);
-      this.editorChangeDebounceTimer = null;
-    }, 500);
-  }
-
-  private async handleCommentTitleClick(comment: Comment): Promise<void> {
-    const words = comment.body.split(" ");
-    const savedAnnotation = await this.annotationService.findCommentBySource(
-      comment.filePath,
-      comment.title,
-      comment.title.split(" ")[0],
-      words[words.length - 1]
-    );
-
-    if (savedAnnotation) {
-      this.showAnnotationFormPanel(comment, "comment", savedAnnotation, true);
-    }
-  }
-
-  private async handleNoteLinkClick(noteLinkInfo: NoteLinkInfo): Promise<void> {
-    // Search for existing annotation by link text
-    const savedAnnotation = await this.annotationService.findNoteByLinkText(
-      noteLinkInfo.filePath,
-      noteLinkInfo.linkText
-    );
-
-    this.showNoteFormPanel(
-      noteLinkInfo,
-      savedAnnotation,
-      !noteLinkInfo.hasTextAround
-    );
-  }
-
-  private async checkCursorInComment(force = false): Promise<void> {
-    const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
-
-    if (!activeView) {
-      return;
-    }
-
-    const mode = activeView.getMode();
-    if (mode !== "source") {
-      return;
-    }
-
-    const editor = activeView.editor;
-    const cursor = editor.getCursor();
-    const cursorLine = cursor.line;
-    const cursorCh = cursor.ch;
-
-    // Check if cursor position changed (line OR character position)
-    if (
-      !force &&
-      cursorLine === this.lastCursorLine &&
-      cursorCh === this.lastCursorCh
-    ) {
-      return;
-    }
-
-    this.lastCursorLine = cursorLine;
-    this.lastCursorCh = cursorCh;
-
-    const file = activeView.file;
-    if (!file) {
-      return;
-    }
-
-    const lineText = editor.getLine(cursorLine);
-
-    // Find comment at cursor position (supports multiple comments per line)
-    const comment = this.commentParser.findCommentAtPosition(
-      lineText,
-      cursorCh,
-      file.name,
-      file.path
-    );
-
-    if (comment) {
-      const words = comment.body.split(" ");
-      const savedAnnotation = await this.annotationService.findCommentBySource(
-        file.path,
-        comment.title,
-        comment.title.split(" ")[0],
-        words[words.length - 1]
-      );
-      this.showAnnotationFormPanel(comment, "comment", savedAnnotation);
-    } else {
-      const noteLinkInfo = detectNoteLink(lineText, file.name, file.path);
-      if (noteLinkInfo) {
-        const linkStart = lineText.indexOf(noteLinkInfo.linkText);
-        const linkEnd = linkStart + noteLinkInfo.linkText.length;
-
-        if (cursorCh >= linkStart && cursorCh <= linkEnd) {
-          return;
-        }
-      }
-
-      const existingPanels =
-        this.app.workspace.getLeavesOfType(ANNOTATION_FORM_VIEW);
-      if (existingPanels.length > 0) {
-        const view = existingPanels[0].view as AnnotationFormView;
-        if (view) {
-          view.clear();
-        }
-      }
-    }
-  }
-
-  private showAnnotationFormPanel(
-    data: Comment,
-    type: "comment",
-    savedAnnotation: Annotation | null = null,
-    openTargetArticle = false
-  ): void {
-    const existingRightPanelLeaves =
-      this.app.workspace.getLeavesOfType(ANNOTATION_FORM_VIEW);
-
-    let rightLeaf;
-    if (existingRightPanelLeaves.length > 0) {
-      rightLeaf = existingRightPanelLeaves[0];
-    } else {
-      rightLeaf = this.app.workspace.getRightLeaf(false);
-      if (rightLeaf) {
-        rightLeaf.setViewState({
-          type: ANNOTATION_FORM_VIEW,
-          active: false,
-        });
-
-        this.app.workspace.rightSplit.expand();
-      }
-    }
-
-    if (rightLeaf) {
-      const view = rightLeaf.view as AnnotationFormView;
-      if (view) {
-        view.updateComment(data as Comment, savedAnnotation, openTargetArticle);
-      }
-    }
-  }
-
-  private showNoteFormPanel(
-    noteLinkInfo: NoteLinkInfo,
-    savedAnnotation: WebAnnotation | null,
-    hideSourceFields: boolean
-  ): void {
-    const existingRightPanelLeaves =
-      this.app.workspace.getLeavesOfType(ANNOTATION_FORM_VIEW);
-
-    let rightLeaf;
-    if (existingRightPanelLeaves.length > 0) {
-      rightLeaf = existingRightPanelLeaves[0];
-    } else {
-      rightLeaf = this.app.workspace.getRightLeaf(false);
-      if (rightLeaf) {
-        rightLeaf.setViewState({
-          type: ANNOTATION_FORM_VIEW,
-          active: true,
-        });
-
-        this.app.workspace.rightSplit.expand();
-      }
-    }
-
-    if (rightLeaf) {
-      const view = rightLeaf.view as AnnotationFormView;
-      if (view) {
-        view.updateNoteWithLinkInfo(
-          noteLinkInfo,
-          savedAnnotation,
-          hideSourceFields
-        );
-      }
-    }
-  }
-
-  private async handleFileChange(): Promise<void> {
-    // Clear processed containers when switching files to allow re-highlighting
-    this.annotationHighlighter.clearProcessedContainers();
-
-    const currentFile = this.app.workspace.getActiveFile();
-    this.previousFile = currentFile;
-  }
-
-  onunload() {
-    // Restore original openLinkText function
-    if (this.restoreLinkOpening) {
-      this.restoreLinkOpening();
-    }
-
-    // Clear cursor check interval
-    if (this.cursorCheckInterval !== null) {
-      window.clearInterval(this.cursorCheckInterval);
-    }
-  }
+  onunload() {}
 }
 
 class IdealogsSettingTab extends PluginSettingTab {
@@ -606,8 +143,5 @@ class IdealogsSettingTab extends PluginSettingTab {
             new Notice("API cache cleared successfully");
           })
       );
-
-    // Annotations section
-    containerEl.createEl("h3", { text: "Annotations" });
   }
 }
--- utils/link-handlers.ts
+++ utils/link-handlers.ts
@@ -1,169 +1,6 @@
-import { App, TFile, MarkdownView } from "obsidian";
-import { WRITING_LINK_PREFIX, COMMON_LINK_PREFIXES } from "../constants";
+import { App, TFile } from "obsidian";
+import { COMMON_LINK_PREFIXES } from "../constants";
 import { ApiService } from "./api";
-import { IdealogsFileTracker } from "./idealogs-file-tracker";
-import { AnnotationService, WebAnnotation } from "./annotation-service";
-import { AnnotationHighlighter } from "./annotation-highlighter";
-import { SplitManager } from "./split-manager";
-
-export class NoteLinkHandler {
-  private app: App;
-  private apiService: ApiService;
-  private fileTracker: IdealogsFileTracker;
-  private annotationService: AnnotationService;
-  private annotationHighlighter: AnnotationHighlighter;
-  private splitManager: SplitManager;
-
-  constructor(
-    app: App,
-    apiService: ApiService,
-    fileTracker: IdealogsFileTracker,
-    annotationService: AnnotationService,
-    annotationHighlighter: AnnotationHighlighter,
-    splitManager: SplitManager
-  ) {
-    this.app = app;
-    this.apiService = apiService;
-    this.fileTracker = fileTracker;
-    this.annotationService = annotationService;
-    this.annotationHighlighter = annotationHighlighter;
-    this.splitManager = splitManager;
-  }
-
-  async handleLink(
-    linkText: string,
-    sourcePath: string,
-    isFromPreviewMode = false
-  ): Promise<void> {
-    try {
-      const atIndex = linkText.indexOf("@");
-      if (atIndex === -1) {
-        console.error("Invalid link format:", linkText);
-        return;
-      }
-
-      const articleId = linkText.substring(atIndex + 1);
-
-      const articleData = await this.apiService.fetchArticleById(articleId);
-
-      const content = await this.apiService.fetchFileContent(articleId);
-
-      const sanitizedTitle = articleData.title.replace(/[/\\:*?"<>|]/g, "");
-      const fileName = `${sanitizedTitle}.md`;
-
-      let file = this.app.vault.getAbstractFileByPath(fileName);
-
-      if (file instanceof TFile) {
-        await this.app.vault.modify(file, content);
-      } else {
-        file = await this.app.vault.create(fileName, content);
-      }
-
-      this.fileTracker.track(fileName, articleId);
-
-      // Open in split using the shared SplitManager
-      await this.splitManager.openInSplit(file as TFile);
-
-      const annotations = await this.annotationService.loadAnnotations(
-        sourcePath
-      );
-      let noteToHighlight: WebAnnotation | null = null;
-
-      for (const noteId in annotations.notes) {
-        const note = annotations.notes[noteId];
-        if (
-          note.target === articleId ||
-          note.target.includes(articleId) ||
-          articleId.includes(note.target)
-        ) {
-          noteToHighlight = note;
-          break;
-        }
-      }
-
-      if (
-        noteToHighlight &&
-        noteToHighlight.target_txt &&
-        noteToHighlight.isValid !== false &&
-        isFromPreviewMode
-      ) {
-        const targetText = noteToHighlight.target_txt;
-        setTimeout(() => {
-          const splitLeaf = this.splitManager.getSplitLeaf();
-          if (splitLeaf) {
-            this.annotationHighlighter.flashTargetText(targetText, splitLeaf);
-          }
-        }, 500);
-      }
-    } catch (error) {
-      console.error("[WritingLinkHandler] Error handling writing link:", error);
-    }
-  }
-
-  private highlightTargetText(targetText: string): void {
-    const splitLeaf = this.splitManager.getSplitLeaf();
-    if (!splitLeaf || !splitLeaf.view) {
-      console.warn("[WritingLinkHandler] No split leaf or view");
-      return;
-    }
-
-    const view = splitLeaf.view;
-    // @ts-ignore - accessing containerEl
-    const container = view.containerEl?.querySelector(".markdown-preview-view");
-
-    if (!container) {
-      console.warn("[WritingLinkHandler] Could not find preview container");
-      return;
-    }
-
-    // Find the target text in the DOM
-    const walker = document.createTreeWalker(
-      container,
-      NodeFilter.SHOW_TEXT,
-      null
-    );
-
-    let currentNode: Node | null;
-    let nodesChecked = 0;
-    while ((currentNode = walker.nextNode())) {
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      nodesChecked++;
-      const textNode = currentNode as Text;
-      const text = textNode.textContent || "";
-      const index = text.indexOf(targetText);
-
-      if (index !== -1) {
-        const beforeText = text.substring(0, index);
-        const matchText = text.substring(index, index + targetText.length);
-        const afterText = text.substring(index + targetText.length);
-
-        const span = document.createElement("span");
-        span.className = "idl-target-flash";
-        span.textContent = matchText;
-
-        const fragment = document.createDocumentFragment();
-        if (beforeText) {
-          fragment.appendChild(document.createTextNode(beforeText));
-        }
-        fragment.appendChild(span);
-        if (afterText) {
-          fragment.appendChild(document.createTextNode(afterText));
-        }
-
-        textNode.parentNode?.replaceChild(fragment, textNode);
-
-        span.scrollIntoView({ behavior: "smooth", block: "center" });
-
-        setTimeout(() => {
-          const textNode = document.createTextNode(matchText);
-          span.parentNode?.replaceChild(textNode, span);
-        }, 2000);
-
-        break;
-      }
-    }
-  }
-}
 
 export class CommonLinkHandler {
   private app: App;
@@ -217,7 +54,6 @@ export class CommonLinkHandler {
 
 export function patchLinkOpening(
   app: App,
-  noteLinkHandler: NoteLinkHandler,
   commonLinkHandler: CommonLinkHandler // Questions & Insights
 ): () => void {
   const workspace = app.workspace;
@@ -230,19 +66,6 @@ export function patchLinkOpening(
     newLeaf?: boolean,
     openViewState?: unknown
   ) {
-    // Detect if preview mode
-    let isFromPreviewMode = false;
-    const activeView = app.workspace.getActiveViewOfType(MarkdownView);
-    if (activeView) {
-      const mode = activeView.getMode();
-      isFromPreviewMode = mode === "preview";
-    }
-
-    if (linktext.startsWith(WRITING_LINK_PREFIX)) {
-      noteLinkHandler.handleLink(linktext, sourcePath, isFromPreviewMode);
-      return;
-    }
-
     for (const prefix of COMMON_LINK_PREFIXES) {
       if (linktext.startsWith(prefix)) {
         commonLinkHandler.handleLink(linktext, sourcePath);
