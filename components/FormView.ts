10bf835f50f1821d58f687181f8da32b8fa769b8
Dan Sto. Domingo <danielesto.domingo@gmail.com>
2025-11-19 01:35:06 +0800

    saving annotation

--- components/FormView.ts
+++ components/FormView.ts
@@ -1,7 +1,9 @@
-import { ItemView, WorkspaceLeaf } from "obsidian";
+import { ItemView, WorkspaceLeaf, App } from "obsidian";
 import { Comment } from "../utils/parsers";
 import { CommentForm } from "./CommentForm";
 import { Article } from "../types";
+import { ApiService } from "../utils/api";
+import { AnnotationService } from "../utils/annotation-service";
 
 export const FORM_VIEW_TYPE = "form-view";
 
@@ -9,6 +11,8 @@ export class FormView extends ItemView {
   private commentForm: CommentForm | null = null;
   private container: HTMLElement | null = null;
   private onArticleSelectedCallback: ((article: Article) => void) | null = null;
+  private apiService: ApiService | null = null;
+  private annotationService: AnnotationService | null = null;
 
   constructor(leaf: WorkspaceLeaf) {
     super(leaf);
@@ -49,6 +53,11 @@ export class FormView extends ItemView {
     this.onArticleSelectedCallback = callback;
   }
 
+  setServices(apiService: ApiService, annotationService: AnnotationService): void {
+    this.apiService = apiService;
+    this.annotationService = annotationService;
+  }
+
   updateComment(comment: Comment): void {
     // Clear existing form if any
     if (this.commentForm) {
@@ -61,9 +70,12 @@ export class FormView extends ItemView {
     }
 
     // Create new form with the detected comment
-    if (this.container) {
+    if (this.container && this.apiService && this.annotationService) {
       this.commentForm = new CommentForm({
         container: this.container,
+        app: this.app,
+        apiService: this.apiService,
+        annotationService: this.annotationService,
         comment: comment,
         onArticleSelected: this.onArticleSelectedCallback || undefined,
       });
